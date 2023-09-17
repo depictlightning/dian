a9ae34bf2d9aa0fce5d7d61c5abefbe86a9e1793
depictlightning <2807450207@qq.com>
2023-09-17 23:12:27 +0800

    完成初始化

--- Level2-1.c
+++ Level2-1.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 int count(int a, int b); //定义距离计算函数
-
 // 输入格式：1 2 3表示初始楼层、目标楼层 和时间	
 //	输入-1 -1 -1表示输入结束 
 //	eg. 5
@@ -8,6 +7,7 @@ int count(int a, int b); //定义距离计算函数
 //		7 4 6
 //		-1 -1 -1
 
+// 
 int main()
 {
 	int beginfloor[10];	//乘客初始楼层
@@ -44,121 +44,77 @@ int main()
 		break;
 		}
 	}
-
-	
-//电梯初始化（前往beginfloor(可能有多个人在0s呼叫）
-	int jilu = 0;			//用于记录还有哪些ti[]的时间没有录入
+//将所有间隔时间转化为总时间（方便与elevatorfloor时间进行比较）
 	for( i=0; i<10; i++){
-		if (ti[i] == 0){
-			begin2floor[i] = beginfloor[i];		//录入初始楼层
-			jilu = i + 1;
+		ti[i+1] += ti[i];
+		if (ti[i+2] < 0){
+		break;	
+		}	
+	}
+	
+//电梯初始化（初始化与后面的代码高度重复但又有细微差别)前往beginfloor(可能有多个人在0s呼叫）
+	while (people == 0){
+		int jilu = 0;			//用于记录还有哪些ti[]的时间没有录入
+		for( i=0; i<10; i++){
+			if (ti[i] == 0){
+				begin2floor[i] = beginfloor[i];		//录入初始楼层
+				jilu = i + 1;
+			}
+			if (ti[i+1] != 0)
+			break;
 		}
-		if (ti[i+1] != 0)
-		break;
-	}
-	int distance =10;
-	int save = 0;
-	for ( i=0; i<10; i++){ 	//第一次只对begin2floor[i]进行比对
-		distance = count(elevatorfloor,begin2floor[i]);
-		if (distance < mindistance){
-			mindistance = distance;
-			closestfloor = begin2floor[i];	//记录最近的楼层
-			save = i;
+		int distance =10;
+		int save = 0;
+		for ( i=0; i<10; i++){ 	//第一次只对begin2floor[i]进行比对
+			distance = count(elevatorfloor,begin2floor[i]);
+			if (distance < mindistance){
+				mindistance = distance;
+				closestfloor = begin2floor[i];	//记录最近的楼层
+				save = i;
+			}
 		}
-	}
-
-//原间隔时间（ti[])与起始终止时间（mindistance)的比较，是否前往原楼层(beginfloor)
-	//大于的情况:正常前往原楼层，删除begin2floor[],录入aim2floor[],people +=1,time +=mindistance
-	if (ti[jilu] > mindistance ){
-		begin2floor[save] = -20;			//删除初始楼层
-		aim2floor[save] = aimfloor[save];	//录入目标楼层
-		elevatorfloor = closestfloor;		//计算楼层
-		people +=1;
-		time += mindistance;
-		printf("%d %d %d\n", elevatorfloor, time, people);		
-	}	
-	//等于的情况：同大于，但同时录入begin2floor[]
-	else if(ti[jilu] == mindistance ){
-		begin2floor[save] = -20;			//删除初始楼层
-		aim2floor[save] = aimfloor[save];	//录入目标楼层
-		elevatorfloor = closestfloor;	//计算楼层
-		people +=1;
-		time += mindistance;
-		printf("%d %d %d\n", elevatorfloor, time, people);
-		save = jilu;						//录入begin2floor
-		jilu ++;
-		begin2floor[save] = beginfloor[save];	
-	}
-	//小于的情况
-	else if (ti[jilu] < mindistance){
-	//基于间隔时间引入testfloor用于判断间隔时间到达后是否需要更改目的楼层
-	int testfloor = 0;
-	//计算testfloor
-	if ( elevatorfloor > closestfloor){	
-		testfloor = elevatorfloor - ti[jilu];
-	}
-	else if( elevatorfloor < closestfloor){
-		testfloor = elevatorfloor + ti[jilu];	
-	}		
-// 比较testlfoor和closestfloor之间距离与mindistance的大小，即引入testdistance
-	int testdistance = 0;
-	testdistance = count(testfloor,closestfloor);
-	mindistance -=ti[jilu];
-		//分三种情况 大于等于时，同ti[jilu] > mindistance的情况：前往begin2floor
-		if (testdistance >= mindistance){
+		int testtime = elevatorfloor + mindistance;	//到达时间后和目前的ti[jilu]进行比较
+		int testfloor;	//用于testtime > ti[jilu]的情况,判断是否前往新呼叫乘客的初始楼层
+		int testdistance;		//比较testdistance 和mindistance的区别判断优先前往哪个楼层
+		
+		if( testtime <= ti[jilu] ){	//正常前往楼层（小于的情况）且是唯一进行printf的	
+			time = testtime;
+			people += 1;
 			begin2floor[save] = -20;			//删除初始楼层
 			aim2floor[save] = aimfloor[save];	//录入目标楼层
-			elevatorfloor = closestfloor;	//计算楼层
-			people += 1;
-			time += mindistance;				//注意time对ti[jilu]的影响
-			ti[jilu] -= testdistance;
+			elevatorfloor = closestfloor;		//计算楼层
 			printf("%d %d %d\n", elevatorfloor, time, people);	
-			save = jilu;						//录入begin2floor
-			jilu ++;
-			begin2floor[save] = beginfloor[save];
-		}	
-		//小于时：前往下一个乘客的初始楼层（
-		else if ( testdistance < mindistance){
-			elevatorfloor = testfloor;
-			time += testdistance;
-			people += 1;
-			begin2floor[save] = beginfloor[save];
-
+		}else if( testtime > ti[jilu] )		//大于的情况，判断是否前往beginfloor
+		{
+			if ( elevatorfloor > closestfloor){	
+				testfloor = elevatorfloor - (ti[jilu]-time);
+			}
+			else if( elevatorfloor < closestfloor){
+				testfloor = elevatorfloor + (ti[jilu]-time);	
+			}		
 		}
+		
+		elevatorfloor = testfloor;			//电梯处于新的楼层
+		testdistance = count(elevatorfloor,beginfloor[jilu]);
+		mindistance = count(elevatorfloor,closestfloor);
+		
+		if (testdistance >= mindistance){	//前往原楼层(但不输出数据）
+			begin2floor[jilu] = beginfloor[jilu];		//录入begin2floor的数据
+			jilu++;									//前往下一个ti[i]								
+		}
+		else if(testdistance < mindistance){	//前往新的楼层
+			mindistance = testdistance;
+			jilu++;
+			begin2floor[jilu] = beginfloor[jilu];		//录入begin2floor的数据
+		}
+	}
 	
-	
-	}		
-
-//// 基于间隔时间引入testfloor用于判断间隔时间到达后是否需要更改目的楼层
-//	int testfloor = 0;
-//	//计算testfloor
-//	if ( elevatorfloor > closestfloor){	
-//		testfloor = elevatorfloor - ti[jilu];
-//	}
-//	else if( elevatorfloor < closestfloor){
-//		testfloor = elevatorfloor + ti[jilu];	
-//	}		
-//// 比较testlfoor和closestfloor之间距离与mindistance的大小，即引入testdistance
-//	int testdistance = 0;
-//	testdistance = count(testfloor,closestfloor);
-//	mindistance -=ti[jilu];
-////分为三种情况：大于，等于，小于
-//
-//	if (testdistance > mindistance){
-//		
-//	}
-//	
-//
-//	
-//
-//
-
 //	//调试
 	printf("\tbegin\tbegin2\taim\taim2\tti\n");
 	for( i=0; i<10; i++){
 			printf("%d\t%d\t%d\t%d\t%d\t%d\n",i,beginfloor[i],begin2floor[i],aimfloor[i],aim2floor[i], ti[i]);
 	}	
-	printf("jilu的值为：%d",jilu);
+	// printf("jilu的值为：%d",jilu);
 	return 0;
 }
 
